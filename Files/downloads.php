674fd20864c907e729a45a8b9095cfa9fe8c1348
Judson Paiva <judson.paiva@hotmail.com>
2023-06-15 23:07:53 +0100

    lendo arquivos com url

---
+++ Files/downloads.php
@@ -0,0 +1,18 @@
+<?php
+
+// $conteudo = file_get_contents('file.txt');
+
+// if ($conteudo) {
+//     echo $conteudo;
+// } else {
+//     echo "Erro ao abrir o arquivo";
+// }
+
+$url = 'https://www.php.net/support';
+$conteudo = file_get_contents($url);
+if ($conteudo) {
+    echo $conteudo;
+    file_put_contents("help.html", $conteudo);
+} else {
+    echo "Erro ao abrir o arquivo";
+}
