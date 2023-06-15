c01d1d15b38d63c458377d5ea19e2d0dba125f16
Judson Paiva <judson.paiva@hotmail.com>
2023-06-15 23:25:22 +0100

    downloads e uploads de arquivos

--- Files/downloads.php
+++ Files/downloads.php
@@ -12,7 +12,7 @@ $url = 'https://www.php.net/support';
 $conteudo = file_get_contents($url);
 if ($conteudo) {
     echo $conteudo;
-    file_put_contents("help.html", $conteudo);
+    file_put_contents("help.html", $conteudo, FILE_APPEND);
 } else {
     echo "Erro ao abrir o arquivo";
 }
---
+++ Files/uploads.php
@@ -0,0 +1,20 @@
+<?php
+    var_dump($_FILES);
+
+?>
+
+
+<html>
+
+<head>
+    <title>Upload de arquivo</title>
+</head>
+
+    <form action="" enctype="multipart/form-data" method="POST">
+        <input type="file" name="arquivo">
+        <input type="submit" value="Enviar Arquivo">
+
+    </form>
+
+
+</html>
